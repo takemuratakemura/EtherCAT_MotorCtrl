82744516149fb7be5189b9593bac1d5140987f7d
takemuratakemura <yuuki_takemura@mail.toyota.co.jp>
2024-05-28 17:23:54 +0900

    Update easycat_test.c

--- easycat_test/easycat_test.c
+++ easycat_test/easycat_test.c
@@ -1,266 +1,117 @@
+2024/5/27生成（DS-402 & アプリケーションマニュアル織り込み済み
+
 #include <stdio.h>
 #include <string.h>
-#include <inttypes.h>
-#include <stdint.h>
+#include <unistd.h>
+#include <ecrt.h>
 
-#include "ethercat.h"
+// EtherCATマスターインスタンス
+static ec_master_t *master = NULL;
+static ec_domain_t *domain1 = NULL;
+static ec_slave_config_t *sc = NULL;
 
-// IOデータバッファ
-static char IOmap[4096];
-// 期待されるWKC値
-static int expectedWKC;
+// PDO entry offsets
+static unsigned int off_control_word;
+static unsigned int off_position_actual_value;
+static unsigned int off_digital_inputs;
+static unsigned int off_status_word;
+static unsigned int off_target_torque;
+static unsigned int off_mode_of_operation;
 
-// 開く
-// nif: ネットワークインターフェースID
-// return: 0=失敗 / 1=成功
-int soem_open(char* nif)
+// 定義
+#define VENDOR_ID 0x00000002
+#define PRODUCT_ID 0x00030924
+#define REVISION_NO 0x00000001
+
+// 更新周期（例：1ms）
+#define PERIOD_NS (1000000)
+
+void cyclic_task(void)
 {
-    int ret = ec_init(nif);
-    return ret;
+    // Domainデータ
+    uint8_t *domain1_pd = ec_domain_data(domain1);
+
+    // 制御ワードとトルク指令を設定
+    EC_WRITE_U16(domain1_pd + off_control_word, 0x000F); // 制御ワード（例：運転イネーブル）
+    EC_WRITE_S16(domain1_pd + off_target_torque, 100);  // トルク指令（例：100）
+
+    // サイクリック同期トルクモードを設定
+    EC_WRITE_S8(domain1_pd + off_mode_of_operation, 0x0A); // モード設定（CST）
+
+    // Process Dataを更新
+    ecrt_domain_queue(domain1);
+    ecrt_master_send(master);
+    ecrt_master_receive(master);
+    ecrt_domain_process(domain1);
 }
 
-// 閉じる
-void soem_close(void)
+int main(int argc, char **argv)
 {
-    ec_close();
-}
-
-#define ALL_SLAVES_OP_STATE 0   // 全てのスレーブがOP状態になりました (成功)
-#define NO_SLAVES_FOUND     1   // スレーブがみつかりません
-#define NOT_ALL_OP_STATE    2   // OP状態にならないスレーブがあります
-
-// コンフィグする
-// return 結果
-int soem_config(void)
-{
-    int oloop, iloop, chk;
-    
-    // スレーブを見つけ、自動コンフィグする
-    if ( ec_config_init(FALSE) > 0 )
-    {
-        printf("%d slaves found and configured.\n",ec_slavecount);
-        
-        ec_config_map(&IOmap);
-        ec_configdc();
-        
-        printf("Slaves mapped, state to SAFE_OP.\n");
-        
-        // 全てのスレーブが SAFE_OP 状態に達するのを待つ
-        ec_statecheck(0, EC_STATE_SAFE_OP,  EC_TIMEOUTSTATE * 4);
-        oloop = ec_slave[0].Obytes;
-        if ((oloop == 0) && (ec_slave[0].Obits > 0)) oloop = 1;
-        if (oloop > 8) oloop = 8;
-        iloop = ec_slave[0].Ibytes;
-        if ((iloop == 0) && (ec_slave[0].Ibits > 0)) iloop = 1;
-        if (iloop > 8) iloop = 8;
-        printf("segments : %d : %d %d %d %d\n",
-            ec_group[0].nsegments,
-            ec_group[0].IOsegment[0],
-            ec_group[0].IOsegment[1],
-            ec_group[0].IOsegment[2],
-            ec_group[0].IOsegment[3]);
-        printf("Request operational state for all slaves\n");
-        expectedWKC = (ec_group[0].outputsWKC * 2) + ec_group[0].inputsWKC;
-        printf("Calculated workcounter %d\n", expectedWKC);
-        
-        // 全てのスレーブにOP状態を要求
-        ec_slave[0].state = EC_STATE_OPERATIONAL;
-        /* send one valid process data to make outputs in slaves happy*/ // ←意味不明
-        ec_send_processdata();
-        ec_receive_processdata(EC_TIMEOUTRET);
-        ec_writestate(0);
-        chk = 40;
-        // 全てのスレーブがOP状態に達するのを待つ
-        do
-        {
-            ec_send_processdata();
-            ec_receive_processdata(EC_TIMEOUTRET);
-            ec_statecheck(0, EC_STATE_OPERATIONAL, 50000);
-        }
-        while (chk-- && (ec_slave[0].state != EC_STATE_OPERATIONAL));
-        
-        if (ec_slave[0].state == EC_STATE_OPERATIONAL )
-        {
-            return ALL_SLAVES_OP_STATE;
-        }
-        else
-        {
-            return NOT_ALL_OP_STATE;
-        }
+    // EtherCATマスターインスタンスの取得
+    master = ecrt_request_master(0);
+    if (!master) {
+        fprintf(stderr, "Failed to acquire master!\n");
+        return -1;
     }
-    else
-    {
-        return NO_SLAVES_FOUND;
+
+    // ドメインの作成
+    domain1 = ecrt_master_create_domain(master);
+    if (!domain1) {
+        fprintf(stderr, "Failed to create domain!\n");
+        return -1;
     }
-}
 
-// スレーブの数を取得
-// return: スレーブの数
-int soem_getSlaveCount(void)
-{
-    return ec_slavecount;
-}
-
-// スレーブの状態を更新
-// return: 全スレーブの中で最も低い状態
-int soem_updateState(void)
-{
-    int ret = ec_readstate();
-    return ret;
-}
-
-// スレーブの状態を取得
-// slave: スレーブのインクリメンタルアドレス
-// return: 状態
-int soem_getState(int slave)
-{
-    return ec_slave[slave].state;
-}
-
-// スレーブのALステータスコードを取得
-// slave: スレーブのインクリメンタルアドレス
-// return: ALステータスコード
-int soem_getALStatusCode(int slave)
-{
-    return ec_slave[slave].ALstatuscode;
-}
-
-// スレーブのALステータスの説明を取得
-// slave: スレーブのインクリメンタルアドレス
-// desc: ALステータスの説明 (最大31文字)
-void soem_getALStatusDesc(int slave, char* desc)
-{
-    snprintf(desc, 31, "%s", ec_ALstatuscode2string( ec_slave[slave].ALstatuscode ));
-}
-
-// スレーブの状態変更を要求
-void soem_requestState(int slave, int state)
-{
-    ec_slave[slave].state = state;
-    ec_writestate(slave);
-}
-
-// スレーブの名前を取得
-// slave: スレーブのインクリメンタルアドレス
-// name: スレーブの名前 (最大31文字)
-void soem_getName(int slave, char* name)
-{
-    snprintf(name, 41, "%s", ec_slave[slave].name );
-}
-
-// スレーブのベンダ番号/製品番号/バージョン番号を取得
-// slave: スレーブのインクリメンタルアドレス
-// id: {ベンダ番号, 製品番号, バージョン番号}
-void soem_getId(int slave, unsigned long* id)
-{
-    id[0] = ec_slave[slave].eep_man;
-    id[1] = ec_slave[slave].eep_id;
-    id[2] = ec_slave[slave].eep_rev;
-}
-
-// PDO転送する
-// return:  0=失敗 / 1=成功
-int soem_transferPDO(void)
-{
-    ec_send_processdata();
-    int wkc = ec_receive_processdata(EC_TIMEOUTRET);
-
-    if(wkc >= expectedWKC){
-        return 1;
-    }else{
-        return 0;
+    // スレーブの構成
+    sc = ecrt_master_slave_config(master, 0, VENDOR_ID, PRODUCT_ID);
+    if (!sc) {
+        fprintf(stderr, "Failed to configure slave!\n");
+        return -1;
     }
+
+    // PDOエントリの設定
+    ec_pdo_entry_reg_t domain1_regs[] = {
+        {0, VENDOR_ID, PRODUCT_ID, 0x6040, 0, &off_control_word},         // Control word
+        {0, VENDOR_ID, PRODUCT_ID, 0x6064, 0, &off_position_actual_value}, // Position actual value
+        {0, VENDOR_ID, PRODUCT_ID, 0x60FD, 0, &off_digital_inputs},       // Digital Inputs
+        {0, VENDOR_ID, PRODUCT_ID, 0x6041, 0, &off_status_word},          // Status word
+        {0, VENDOR_ID, PRODUCT_ID, 0x6071, 0, &off_target_torque},        // Target Torque
+        {0, VENDOR_ID, PRODUCT_ID, 0x6060, 0, &off_mode_of_operation},    // Modes of Operation
+        {}
+    };
+
+    if (ecrt_domain_reg_pdo_entry_list(domain1, domain1_regs)) {
+        fprintf(stderr, "PDO entry registration failed!\n");
+        return -1;
+    }
+
+    // マスターのアクティブ化
+    if (ecrt_master_activate(master)) {
+        fprintf(stderr, "Master activation failed!\n");
+        return -1;
+    }
+
+    // ドメインプロセスデータの取得
+    if (!(domain1_pd = ecrt_domain_data(domain1))) {
+        fprintf(stderr, "Failed to get domain data pointer!\n");
+        return -1;
+    }
+
+    // デバイス初期化シーケンス
+    EC_WRITE_U16(domain1_pd + off_control_word, 0x0080); // エラーリセット
+    usleep(10000); // 10ms待機
+    EC_WRITE_U16(domain1_pd + off_control_word, 0x0006); // スイッチオン準備
+    usleep(10000); // 10ms待機
+    EC_WRITE_U16(domain1_pd + off_control_word, 0x0007); // スイッチオン
+    usleep(10000); // 10ms待機
+    EC_WRITE_U16(domain1_pd + off_control_word, 0x000F); // 運転イネーブル
+    usleep(10000); // 10ms待機
+
+    // サイクルタスクの実行
+    while (1) {
+        cyclic_task();
+        usleep(PERIOD_NS / 1000);
+    }
+
+    return 0;
 }
 
-// 汎用スレーブPDO入力
-// slave: スレーブのインクリメンタルアドレス
-// offset: オフセットアドレス
-// return: 入力値
-uint8_t soem_getInputPDO(int slave, int offset)
-{
-    uint8_t ret = 0;
-    
-    if(slave <= ec_slavecount)
-    {
-        ret = ec_slave[slave].inputs[offset];
-    }
-    return ret;
-}
-
-// 汎用スレーブPDO出力
-// slave: スレーブのインクリメンタルアドレス
-// offset: オフセットアドレス
-// value: 出力値
-void soem_setOutPDO(int slave, int offset, uint8_t value)
-{
-    if(slave <= ec_slavecount)
-    {
-        ec_slave[slave].outputs[offset] = value;
-    }
-}
-
-// メイン関数
-// コマンド引数 : ネットワークインターフェース名 (eth0など)
-int main(int argc, char *argv[])
-{
-    // 引数チェック
-    if (argc > 1)
-    {
-        // ネットワークインターフェース
-        char *ifname = argv[1];
-        
-        // SOEMの初期化
-        int result = soem_open(ifname);
-        if(result == 0)
-        {
-            printf("can not open network adapter!\n");
-            return (1);
-        }
-        result = soem_config();
-        if(result == NO_SLAVES_FOUND)
-        {
-            printf("slaves not found!\n");
-            return (2);
-        }
-        if(result == NOT_ALL_OP_STATE)
-        {
-            printf("at least one slave can not reach OP state!\n");
-            return (3);
-        }
-        
-        // メインループ
-        uint16_t vol = 512;
-        uint8_t servo = 90;
-        while (1)
-        {
-            // 2個目のスレーブのOUTの0バイトめにサーボの指令値を設定
-            soem_setOutPDO(2, 0, servo);
-            // PDO転送
-            soem_transferPDO();
-            // 1個目のスレーブのINの0～1バイトめからボリュームの値を取得する
-            uint8_t h = soem_getInputPDO(1, 0);
-            uint8_t l = soem_getInputPDO(1, 1);
-            vol = ((uint16_t)h << 8) | (uint16_t)l;
-            // ボリュームの値からサーボの指令値に換算
-            servo = (uint8_t)((int)vol * 180 / 1024);
-        }
-        
-        // 切断処理
-        soem_requestState(0, EC_STATE_INIT);
-        soem_close();
-    }
-    else
-    {
-        printf("Available adapters\n");
-        ec_adaptert *adapter = NULL;
-        adapter = ec_find_adapters();
-        while (adapter != NULL)
-        {
-            printf("Description : %s, Device to use for wpcap: %s\n",
-                adapter->desc,
-                adapter->name);
-            adapter = adapter->next;
-        }
-    }
-    return (0);
-}
